91da8005f8a468341593a0e00fed4bc03531ae91
Aaron Piotrowski <fxapps.piotrowski@gmail.com>
2013-06-22 00:33:06 -0400

    Changed the way work outs are displayed. Now using tabs instead.

---
+++ FitGen/src/com/fxapps/fitgen/Mass_Button_2.java
@@ -0,0 +1,16 @@
+package com.fxapps.fitgen;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Mass_Button_2 extends Fragment {
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		   // Inflate the layout for this fragment
+		   return inflater.inflate(R.layout.activity_mass__button_21, container, false);
+		  }
+	}
+
---
+++ FitGen/src/com/fxapps/fitgen/Mass_Button_24.java
@@ -0,0 +1,5 @@
+package com.fxapps.fitgen;
+
+public class Mass_Button_24 {
+
+}

7d321ff863c80c0716f4711c44fc76887ffcab72
Aaron Piotrowski <fxapps.piotrowski@gmail.com>
2013-07-29 19:50:10 -0400

    Added GUI for Custom Workout activity

---
+++ FitGen/src/com/fxapps/fitgen/Custom_Button.java
@@ -0,0 +1,45 @@
+package com.fxapps.fitgen;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+	
+	public class Custom_Button extends Activity {
+		
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.custom_button);
+			
+			ActionBar actionBar = getActionBar();
+		    actionBar.setDisplayHomeAsUpEnabled(true);
+		
+						
+		}
+		
+		@Override
+		public boolean onCreateOptionsMenu(Menu menu) {
+			// Inflate the menu; this adds items to the action bar if it is present.
+			getMenuInflater().inflate(R.menu.mass__button, menu);
+			return true;
+		}
+		public boolean onOptionsItemSelected(MenuItem item) {
+		    switch (item.getItemId()) {
+		        case R.id.gym_find:
+		            Intent intent = new Intent(this, GymLocator.class);
+		            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		            startActivity(intent);
+		            return true;
+		        default:
+		            return super.onOptionsItemSelected(item);
+		    }
+		};
+		}
+		
+
+		
+
+

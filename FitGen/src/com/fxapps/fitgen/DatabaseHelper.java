6ade25a73e8f35fdeba6750a12c856e88eef446a
Aaron Piotrowski <fxapps.piotrowski@gmail.com>
2013-09-29 17:21:02 -0400

    Database helper started

---
+++ FitGen/src/com/fxapps/fitgen/DatabaseHelper.java
@@ -0,0 +1,59 @@
+package com.fxapps.fitgen;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+	
+	static final String dbName = "ExerciseDB";
+	static final String chestTable = "Chest Exercises";
+	static final String colChestID = "exChestName";
+	static final String colChestSet = "setChestNumber";
+	static final String colChestRep = "repChestNumber";
+	
+	static final String armTable = "Arm Exercises";
+	static final String colArmID = "exArmName";
+	static final String colArmSet = "setArmNumber";
+	static final String colArmRep = "repArmNumber";
+	
+	static final String backTable = "Back Exercises";
+	static final String colBackID = "exBackName";
+	static final String colBackSet = "setBackNumber";
+	static final String colBackRep = "repBackNumber";
+	
+	static final String shoulderTable = "Shoulder Exercises";
+	static final String colShoulderID = "exShoulderName";
+	static final String colShoulderSet = "setShoulderNumber";
+	static final String colShoulderRep = "repShoulderNumber";
+	
+	static final String legTable = "Leg Exercises";
+	static final String colLegID = "exLegName";
+	static final String colLegSet = "setLegNumber";
+	static final String colLegRep = "repLegNumber";
+	
+	
+
+public DatabaseHelper(Context context) {
+	super(context, dbName, null, 33);
+}
+
+public void onCreate(SQLiteDatabase db) {
+	
+	db.execSQL("CREATE TABLE "+chestTable+" ("+colChestID+ " TEXT , "+colChestSet+ "INTEGER PRIMARY KEY , "+colChestRep+ "INTEGER PRIMARY KEY)");
+	
+	db.execSQL("CREATE TABLE "+armTable+" ("+colArmID+ "TEXT , "+colArmSet+ "INTEGER PRIMARY KEY , "+colArmRep+ "INTEGER PRIMARY KEY)");
+	
+	db.execSQL("CREATE TABLE "+backTable+" ("+colBackID+ "TEXT , "+colBackSet+ "INTEGER PRIMARY KEY , "+colBackRep+ "INTEGER PRIMARY KEY)");
+	
+	db.execSQL("CREATE TABLE "+shoulderTable+" ("+colShoulderID+ "TEXT , "+colShoulderSet+ "INTEGER PRIMARY KEY , "+colShoulderRep+ "INTEGER PRIMARY KEY)");
+	
+	db.execSQL("CREATE TABLE "+legTable+" ("+colLegID+ "TEXT , "+colLegSet+ "INTEGER PRIMARY KEY , "+colLegRep+ "INTEGER PRIMARY KEY)");
+}
+
+@Override
+public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	// TODO Auto-generated method stub
+	
+}
+}
